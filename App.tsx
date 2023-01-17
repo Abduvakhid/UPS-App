2f793d4c1e838b5b4cd4749b5493386b4c2e691e
Abduvakhid <hendersonhenry249@gmail.com>
2023-01-17 13:40:37 +0500

    completed clone

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { TailwindProvider } from "tailwind-rn";
+import RootNavigator from "./navigator/RootNavigator";
+import utilities from "./tailwind.json";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5001/api/dining-wombat",
+  // uri: "https://dashboard.stepzen.com/explorer?endpoint=api%2Fdining-wombat",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // @ts-ignore - TailwindProvider is missing a type definition
+    <TailwindProvider utilities={utilities}>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
+    </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
